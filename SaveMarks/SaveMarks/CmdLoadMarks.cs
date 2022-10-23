91d2afc9786a7d8324b3da03d2feb76f4852e00a
dflat2 <116065652+dflat2@users.noreply.github.com>
2022-10-23 20:19:19 +0200

    /savemarks code layout

---
+++ SaveMarks/SaveMarks/CmdLoadMarks.cs
@@ -0,0 +1,19 @@
+﻿namespace SaveMarks;
+using System;
+using MCGalaxy;
+
+public class CmdSaveMarks : Command
+{
+    public override string name => "savemarks";
+    public override string type => "building";
+
+    public override void Help(Player p)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Use(Player p, string message)
+    {
+        throw new NotImplementedException();
+    }
+}

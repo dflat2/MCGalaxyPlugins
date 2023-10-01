943378c070885446ffe00f77e567dd9ba2251af8
dflat2 <116065652+dflat2@users.noreply.github.com>
2023-10-01 13:35:38 +0200

    PlacePlugin: draft

---
+++ OneBlockPerMinute/OneBlockPerMinutePlugin/CmdOneBlockPerMinute.cs
@@ -0,0 +1,21 @@
+﻿using MCGalaxy;
+
+namespace PluginOneBlockPerMinute;
+
+public class CmdOneBlockPerMinute : Command
+{
+    public override string name => "OneBlockPerMinute";
+    public override string type => CommandTypes.World;
+    public override bool SuperUseable => false;
+
+    public override void Help(Player player)
+    {
+        player.Message("&T/OneBlockPerMinute enable/disable");
+        player.Message("&H[De]activates 1BPM on current level.");
+    }
+
+    public override void Use(Player p, string message)
+    {
+        
+    }
+}
---
+++ OneBlockPerMinute/OneBlockPerMinutePlugin/OneBlockPerMinutePlugin.cs
@@ -0,0 +1,20 @@
+﻿using MCGalaxy;
+
+namespace PluginOneBlockPerMinute;
+
+public class OneBlockPerMinutePlugin : Plugin
+{
+    public override string name => "OneBlockPerMinute";
+    public override string creator { get { return "D_Flat"; } }
+    public override string MCGalaxy_Version { get { return "1.9.4.3"; } }
+
+    public override void Load(bool auto)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Unload(bool auto)
+    {
+        throw new System.NotImplementedException();
+    }
+}

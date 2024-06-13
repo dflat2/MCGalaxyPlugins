2a8213806fe0ce5597dd66982cc6fdc7f16de0ba
dflat2 <116065652+dflat2@users.noreply.github.com>
2024-06-13 17:38:51 +0200

    first draft for block db truncate plugin

---
+++ BlockDBClean/BlockDBCleanPlugin/BlockDBCleanPlugin.cs
@@ -0,0 +1,81 @@
+namespace PluginBlockDBClean;
+
+using System;
+using System.IO;
+using MCGalaxy;
+using MCGalaxy.DB;
+using MCGalaxy.Maths;
+
+public sealed class BlockDBCleanPlugin : Plugin
+{
+    public override string name { get { return "BlockDBClean"; } }
+    public override string creator { get { return "D_Flat"; } }
+    public override string MCGalaxy_Version { get { return "1.9.4.9"; } }
+
+    private Command _cmdBlockDBClean;
+
+    public BlockDBCleanPlugin() {
+        _cmdBlockDBClean = new CmdBlockDBClean(this);
+    }
+
+    public override void Load(bool startup)
+    {
+        Command.Register(_cmdBlockDBClean);
+    }
+
+    public override void Unload(bool shutdown)
+    {
+        Command.Unregister(_cmdBlockDBClean);
+    }
+
+    public override void Help(Player p)
+    {
+        // TODO
+    }
+
+    public unsafe void Truncate(BlockDB blockDB, TimeSpan delta) {
+        string filePath = BlockDBFile.FilePath(blockDB.MapName);
+        string tempPath = BlockDBFile.TempPath(blockDB.MapName);
+
+        DateTime thresholdDateTime = DateTime.UtcNow - delta;
+        int thresholdIntEpochs = (int)(thresholdDateTime - BlockDB.Epoch).TotalSeconds;
+
+        using (Stream source = File.OpenRead(filePath), destination = File.Create(tempPath)) {
+            Vec3U16 dims;
+            BlockDBFile.ReadHeader(source, out dims);
+            BlockDBFile.WriteHeader(destination, blockDB.Dims);
+            byte[] read = new byte[BlockDBFile.BulkEntries * BlockDBFile.EntrySize];
+            byte[] write = new byte[BlockDBFile.BulkEntries * BlockDBFile.EntrySize];
+            int written;
+
+            fixed (byte* pointer = read, writePointer = write) {
+                BlockDBEntry* entryPointer = (BlockDBEntry*)pointer;
+                while (true) {
+                    written = 0;
+
+                    int count = BlockDBFile.V1.ReadForward(source, read, entryPointer);
+
+                    if (count == 0) {
+                        break;
+                    }
+
+                    for (int i = count; i < count; i++) {
+                        if (entryPointer[i].TimeDelta >= thresholdIntEpochs) {
+                            Buffer.MemoryCopy(
+                                &entryPointer[i],
+                                writePointer + written * BlockDBFile.EntrySize,
+                                BlockDBFile.EntrySize,
+                                BlockDBFile.EntrySize);
+                            written++;
+                        }
+                    }
+
+                    destination.Write(write, 0, written * BlockDBFile.EntrySize);
+                }
+            }
+        }
+
+        File.Delete(filePath);
+        File.Move(tempPath, filePath);
+    }
+}
---
+++ BlockDBClean/BlockDBCleanPlugin/CmdBlockDBClean.cs
@@ -0,0 +1,53 @@
+﻿namespace PluginBlockDBClean;
+using System;
+using MCGalaxy;
+using MCGalaxy.Commands;
+
+public class CmdBlockDBClean : Command {
+    public override string name => "BlockDBClean";
+    public override string type => CommandTypes.World;
+    public override LevelPermission defaultRank { get { return LevelPermission.Admin; } }
+    private readonly BlockDBCleanPlugin _blockDBCleanPlugin;
+
+
+    public CmdBlockDBClean(BlockDBCleanPlugin blockDBCleanPlugin) {
+        _blockDBCleanPlugin = blockDBCleanPlugin;
+    }
+
+    public override void Use(Player player, string message) {
+        string[] arguments = message.SplitSpaces();
+
+        if (arguments.Length != 2) {
+            player.Message("Usage: &T/BlockDBClean <map> <duration>&S.");
+            return;
+        }
+
+        string mapName = Matcher.FindMaps(player, arguments[0]);
+
+        if (mapName == null) {
+            return;
+        }
+
+        Level level = LevelInfo.FindExact(mapName);
+
+        if (level == null) {
+            player.Message(string.Format("Cannot clean BlockDB for {0} because it is currently unloaded.", mapName));
+            return;
+        }
+
+        TimeSpan delta = TimeSpan.FromDays(365);
+
+        if (!CommandParser.GetTimespan(player, arguments[1], ref delta, "truncate the past", "s")) {
+            return;
+        }
+
+        _blockDBCleanPlugin.Truncate(level.BlockDB, delta);
+        player.Message("Done.");
+    }
+
+    public override void Help(Player player) {
+        player.Message("&T/BlockDBClean <map> <duration>");
+        player.Message("Truncates the BlockDB on given &Tmap&S.");
+        player.Message("Only keeps records for the past &Tduration&S.");
+    }
+}

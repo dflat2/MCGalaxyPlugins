93848de1ff23d31c55502c1581458f09deb08c3f
dflat2 <116065652+dflat2@users.noreply.github.com>
2024-07-21 18:04:30 +0200

    removed `MarksSaver` plugin
    Removed `MarksSaver` plugin as there is already and MCGalaxy `/ZoneMark`
    command doing almost the exact same thing.

--- MarksSaver/MarksSaverPlugin/MarksSaverUtils.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PluginMarksSaver;
-using MCGalaxy.Maths;
-
-public static class SaveMarksUtils {
-    public static string ToStringNoComma(this Vec3S32 vector) {
-        return $"{vector.X} {vector.Y} {vector.Z}";
-    }
-}
